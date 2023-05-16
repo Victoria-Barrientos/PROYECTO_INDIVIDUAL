49456649f0c55ac1df335302b8bfa5a0660ee851
Victoria-Barrientos <viictoriabarrientos@gmail.com>
2023-05-16 10:04:13 -0300

    Added routes and rendered components

--- Barrientos-PI/client/src/App.js
+++ Barrientos-PI/client/src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import Nav from './components/Nav/Nav';
+import Landing from './components/Landing/Landing';
+import Home from './components/Home/Home';
+import Detail from './components/Detail/Detail';
+import NewGame from './components/NewGame';
 
 function App() {
+
+  const location = useLocation();
+
   return (
     <div className="App">
-      <h1>Henry Videogames</h1>
+      {
+         location.pathname !== "/" && <Nav/>
+      }
+      <Routes>
+        <Route path="/" element={<Landing></Landing>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/detail/:id" element={<Detail></Detail>}></Route>
+        <Route path="/new" element={<NewGame></NewGame>}></Route>
+      </Routes>
     </div>
   );
 }
