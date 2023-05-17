2dddab92290d002a3b915bf8e206e9344a06ccd0
Victoria-Barrientos <viictoriabarrientos@gmail.com>
2023-05-17 11:18:40 -0300

    Added new game button

--- Barrientos-PI/client/src/components/Nav/Nav.jsx
+++ Barrientos-PI/client/src/components/Nav/Nav.jsx
@@ -5,7 +5,10 @@ export default function Nav() {
   return (
     <div className={styles.container}>
       <Link to='/' className={styles.link}><h1 className={styles.logo}>Pixel Playground</h1></Link>
-      <button className={styles.navButton} id="loginBtn">Login</button>
+      <div className={styles.navButtonContainer}>
+      <Link to='/new' className={styles.link}><h1 className={styles.newButton}>+</h1></Link>
+      <Link to='/login' className={styles.link}><button className={styles.logButton} id="loginBtn">Login</button></Link>
+      </div>
     </div>
   );
 }
