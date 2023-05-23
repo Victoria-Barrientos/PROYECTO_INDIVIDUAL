b582a1dd59d09d0908e989a95f2129c34d7d451b
Victoria-Barrientos <viictoriabarrientos@gmail.com>
2023-05-23 15:39:45 -0300

    Nav styles clean up

--- Barrientos-PI/client/src/components/Nav/Nav.jsx
+++ Barrientos-PI/client/src/components/Nav/Nav.jsx
@@ -2,13 +2,15 @@ import { Link } from 'react-router-dom';
 import styles from "./Nav.module.css";
 
 export default function Nav() {
+
   return (
     <div className={styles.container}>
-      <Link to='/' className={styles.link}><h1 className={styles.logo}>Pixel Playground</h1></Link>
-      <div className={styles.navButtonContainer}>
-      <Link to='/saved' className={styles.link}><h1 className={styles.savedButton}>Saved</h1></Link>
-      <Link to='/new' className={styles.link}><h1 className={styles.newButton}>+</h1></Link>
-      <Link to='/login' className={styles.link}><button className={styles.logButton} id="loginBtn">Login</button></Link>
+      <Link to='/' className={styles.link}><h1 className={styles.logo}>PIXELS PLAYGROUND</h1></Link>
+      <div className={styles.buttonContainer}>
+      <Link to='/home' className={styles.link}><h1 className={styles.button}>Home</h1></Link>
+      <Link to='/saved' className={styles.link}><h1 className={styles.button}>Saved</h1></Link>
+      <Link to='/new' className={styles.link}><h1 className={styles.button}>New</h1></Link>
+      <Link to='/login' className={styles.link}><h1 className={styles.button} id="loginBtn">Login</h1></Link>
       </div>
     </div>
   );
