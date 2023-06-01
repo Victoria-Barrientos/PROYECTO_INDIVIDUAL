2c44d305f8e101718b3c1273028dbe9ee9dfbd0f
Victoria-Barrientos <viictoriabarrientos@gmail.com>
2023-06-01 16:11:36 -0300

    deploy

--- Barrientos-PI/api/index.js
+++ Barrientos-PI/api/index.js
@@ -1,9 +1,10 @@
 const server = require('./src/app.js');
+const PORT = process.env.PORT || 3001
 const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
-    console.log('backend listening at 3001'); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log(`Server listening at ${PORT}`); // eslint-disable-line no-console
   });
 });
